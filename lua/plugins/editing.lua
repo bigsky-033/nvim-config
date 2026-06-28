36f49c9f14035d233b65bdc39cca79d346848b68
bigsky-033 <bigsky-033@proton.me>
2026-06-28 19:56:39 +0900

    Add autopairs and indent guides

---
+++ lua/plugins/editing.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "windwp/nvim-autopairs",
+    event = "InsertEnter",
+    opts = {},
+  },
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    event = { "BufReadPost", "BufNewFile" },
+    opts = {
+      indent = { char = "│" },
+      scope = { enabled = false },
+    },
+  },
+}
