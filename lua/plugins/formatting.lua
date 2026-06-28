84bdd57f31d1671b90dfdad8ca44858a5469e15c
bigsky-033 <bigsky-033@proton.me>
2026-06-28 19:43:27 +0900

    Add conform formatting with mason-tool-installer

---
+++ lua/plugins/formatting.lua
@@ -0,0 +1,40 @@
+return {
+  "stevearc/conform.nvim",
+  event = { "BufWritePre" },
+  cmd = { "ConformInfo" },
+  dependencies = {
+    {
+      "WhoIsSethDaniel/mason-tool-installer.nvim",
+      dependencies = { "mason-org/mason.nvim" },
+      opts = {
+        ensure_installed = { "stylua", "ruff", "prettierd", "shfmt", "taplo" },
+      },
+    },
+  },
+  opts = {
+    formatters_by_ft = {
+      lua = { "stylua" },
+      python = { "ruff_organize_imports", "ruff_format" },
+      javascript = { "prettierd" },
+      typescript = { "prettierd" },
+      javascriptreact = { "prettierd" },
+      typescriptreact = { "prettierd" },
+      json = { "prettierd" },
+      jsonc = { "prettierd" },
+      yaml = { "prettierd" },
+      markdown = { "prettierd" },
+      css = { "prettierd" },
+      html = { "prettierd" },
+      sh = { "shfmt" },
+      bash = { "shfmt" },
+      toml = { "taplo" },
+    },
+    format_on_save = { timeout_ms = 1000, lsp_format = "fallback" },
+  },
+  config = function(_, opts)
+    require("conform").setup(opts)
+    vim.keymap.set({ "n", "v" }, "<leader>cf", function()
+      require("conform").format({ async = false, lsp_format = "fallback" })
+    end, { desc = "Format buffer/selection" })
+  end,
+}
